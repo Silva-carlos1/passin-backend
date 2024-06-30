a26f410e60a02b648e7caadd8b07324de41a64ff
Carlos <silva.carloseduardo.ds@gmail.com>
2024-06-30 12:00:14 -0300

    feat: add check-in feature for attendees

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import z, { object } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function getAttendeeBadge(app: FastifyInstance) {
@@ -9,7 +9,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
       params: z.object({
         attendeeId: z.coerce.number().int()
       }),
-
+      response: {
+        200: z.object({
+          badge: z.object({
+            name: z.string(),
+            email: z.string().email(),
+            eventTitle: z.string(),
+            checkInURL: z.string().url(),
+          })
+        })
+      }
     }
   }, async (request, reply) => {
     const { attendeeId } = request.params
@@ -33,6 +42,17 @@ export async function getAttendeeBadge(app: FastifyInstance) {
       throw new Error("attendee not found.")
     }
 
-    return reply.send({ attendee })
+    const baseURL = `${request.protocol}://${request.hostname}`
+
+    const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+    return reply.send({
+      badge: {
+        name: attendee.name,
+        email: attendee.email,
+        eventTitle: attendee.event.title,
+        checkInURL: checkInURL.toString()
+      }
+     })
   })
 }
